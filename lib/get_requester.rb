b8f7cf216aaaa54e18646229280cd1c10f5cf35f
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-10 22:51:38 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,20 +3,11 @@ require 'open-uri'
 require 'json'
 require 'pry'
 
-# uri = URI.parse(url)
-# response = Net::HTTP.get_response(uri)
-# response.body
-
-
-
-
 class GetRequester
   
-  # URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-  
-def initialize(url)
-  uri = URI.parse(url)
-end
+  def initialize(url)
+    uri = URI.parse(url)
+  end
   
   
   def get_response_body
@@ -28,12 +19,6 @@ end
   def parse_json 
     uri = URI.parse(URL)
     people = JSON.parse(self.get_response_body)
-    # hash = {}
-      # people.collect do |people| 
-      #   people["name"]
-      #   # people["occupation"]
-      # end
-    
   end
 
 end
