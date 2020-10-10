0a1eea052bca69dd4e8ddb033d34e1a54c98b970
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-10 22:25:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,37 @@
-# Write your code here
+require 'net/http'
+require 'json'
+require 'open-uri'
+require 'pry'
+
+
+
+
+class GetRequester
+  
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  
+  
+  def get_response_body
+    # uri = URI.parse(URL)  
+    # response = Net::HTTP.get_response(uri)
+    # response.body 
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end 
+  
+  def get_programs
+  uri = URI.parse(URL)
+  response = Net::HTTP.get_response(uri)
+  response.body
+end
+  
+  def parse_json 
+    uri = URI.parse(URL)
+    programs = JSON.parse(self.get_resonse_body)
+    programs.collect do |program| 
+      program["agency"]
+    end 
+  end
+
+end
