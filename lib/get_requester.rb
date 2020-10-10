d28b4d8964a8c0bfca308a67c201a93d78ddfc9c
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-10 22:31:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -23,11 +23,11 @@ end
   
 #   def get_response_body
 #     # uri = URI.parse(URL)  
-#     # response = Net::HTTP.get_response(uri)
-#     # response.body 
-#     uri = URI.parse(URL)
-#     response = Net::HTTP.get_response(uri)
-#     response.body
+    # response = Net::HTTP.get_response(uri)
+    # response.body 
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
 #   end 
   
 #   def get_programs
