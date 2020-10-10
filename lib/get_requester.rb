29aa3385ed2452a4446e1c42a250d7b711bc80ab
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-10 22:28:18 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,28 +14,34 @@ class GetRequester
   
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
-  
-  def get_response_body
-    # uri = URI.parse(URL)  
-    # response = Net::HTTP.get_response(uri)
-    # response.body 
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end 
-  
-  def get_programs
-  uri = URI.parse(URL)
+def initialize(url)
+  uri = URI.parse(url)
   response = Net::HTTP.get_response(uri)
   response.body
 end
   
-  def parse_json 
-    uri = URI.parse(URL)
-    programs = JSON.parse(self.get_resonse_body)
-    programs.collect do |program| 
-      program["agency"]
-    end 
-  end
+  
+#   def get_response_body
+#     # uri = URI.parse(URL)  
+#     # response = Net::HTTP.get_response(uri)
+#     # response.body 
+#     uri = URI.parse(URL)
+#     response = Net::HTTP.get_response(uri)
+#     response.body
+#   end 
+  
+#   def get_programs
+#   uri = URI.parse(URL)
+#   response = Net::HTTP.get_response(uri)
+#   response.body
+# end
+  
+#   def parse_json 
+#     uri = URI.parse(URL)
+#     programs = JSON.parse(self.get_resonse_body)
+#     programs.collect do |program| 
+#       program["agency"]
+#     end 
+#   end
 
 end
